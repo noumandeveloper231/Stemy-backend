10aaed8ce877505ee26541e196574ff5a7cd6915
Nouman <nt50616849@gmail.com>
2026-04-29 10:34:57 +0500

    first commit

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,7 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import PrismaPkg from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 import { env } from "../config/env.js";
+
+const { PrismaClient } = PrismaPkg;
 
 const { Pool } = pg;
 const pool = new Pool({
